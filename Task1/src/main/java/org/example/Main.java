b6f7f31e7d3e9d39a19370d437d80e95335451c4
NataliaLuberda <nluberda@student.agh.edu.pl>
2023-04-23 21:52:02 +0200

    Upload Task1

--- Task1/src/main/java/org/example/Main.java
+++ Task1/src/main/java/org/example/Main.java
@@ -1,7 +1,32 @@
 package org.example;
 
 public class Main {
+
+    public static Tree makingTree(){
+        Node node1 = new Node(5);
+        Tree tree = new Tree(node1);
+
+        tree.addNodeWithValue(3,5);
+        tree.addNodeWithValue(7,5);
+
+        tree.addNodeWithValue(2,3);
+        tree.addNodeWithValue(5,3);
+
+
+        tree.addNodeWithValue(1,7);
+        tree.addNodeWithValue(0,7);
+
+
+        tree.addNodeWithValue(2,0);
+        tree.addNodeWithValue(8,0);
+
+        return tree;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Tree newTree = makingTree();
+        newTree.printTree();
+        System.out.print("Root has value " + newTree.getRoot().value);
+
     }
 }
